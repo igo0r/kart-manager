09049f21837a25ed2c5addbb9c61e8ed9b94256f
igorlantushenko <igor.lantushenko@gmail.com>
2021-08-13 17:11:43 +0300

    less logs

--- main.js
+++ main.js
@@ -158,6 +158,7 @@ function parseApexData(data) {
 function parseData(data) {
    // console.log(data);
     //console.log(storage);
+    console.log('Response received');
     let adapter = getAdapter();
     let needToRecalculate = false;
     if (!data[adapter.data]) {
@@ -172,17 +173,18 @@ function parseData(data) {
         //Pitstop
         if (item[adapter.pitTime] && item[adapter.pitTime] >= 0) {
             console.log(`${item[adapter.teamName]} is in PIT for ${item[adapter.pitTime]}`);
-            !storage.teams[item[adapter.teamName]]['pitstop'] ? pitStop(item[adapter.teamName]) : '';
-            storage.teams[item[adapter.teamName]]['pitstop'] = true;
+            !storage.teams[item[adapter.teamName]].pitstop ? pitStop(item[adapter.teamName]) : '';
+            storage.teams[item[adapter.teamName]].pitstop = true;
             console.log(`Set empty laps for ${item[adapter.teamName]}`);
             storage.teams[item[adapter.teamName]].laps = [];
+            storage.teams[item[adapter.teamName]].stint = 0;
             needToRecalculate = true;
         } else if ((team.laps.length === 0 || team['last_lap'] !== item[adapter.lapNumber]) && (item[adapter.lapTime] > 0 && item[adapter.lapTime] < 125000)) {
             console.log(`${item[adapter.teamName]} has new lap ${item[adapter.lapNumber]} with time: ${item[adapter.lapTime]}`);
             storage.teams[item[adapter.teamName]]['last_lap'] = item[adapter.lapNumber];
-            storage.teams[item[adapter.teamName]]['kart'] = storage.track === '2g' ? item["Kart"]["Name"] : adapter.kart;
-            storage.teams[item[adapter.teamName]]['pitstop'] = false;
-            storage.teams[item[adapter.teamName]]['stint'] = item[adapter.stint] ? item[adapter.stint] : 0;
+            storage.teams[item[adapter.teamName]].kart = storage.track === '2g' ? item["Kart"]["Name"] : adapter.kart;
+            storage.teams[item[adapter.teamName]].pitstop = false;
+            storage.teams[item[adapter.teamName]].stint = item[adapter.stint] ? item[adapter.stint] : 0;
             storage.teams[item[adapter.teamName]].laps.push({
                 lap: item[adapter.lapNumber],
                 "lap_time": item[adapter.lapTime],
